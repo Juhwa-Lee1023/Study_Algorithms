cb52ecc708a447287486ba9efa35111ad36ce187
Juhwa-Lee1023 <63584245+Juhwa-Lee1023@users.noreply.github.com>
2022-05-04 01:11:23 +0900

    [Bronze V] Title: 사칙연산, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+ 
+int main(int argc, char const *argv[]) {
+    int x, y;
+ 
+    cin >> x >> y;
+    
+    cout << x+y << "\n";
+    cout << x-y << "\n";
+    cout << x*y << "\n";
+    cout << x/y << "\n";
+    cout << x%y << "\n";
+    return 0;
+}
