eee2a7c2fd520bc87a72cfd164d488db5eacf86e
Juhwa-Lee1023 <63584245+Juhwa-Lee1023@users.noreply.github.com>
2022-05-06 15:09:55 +0900

    [Bronze III] Title: 별 찍기 - 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+	
+
+
+int main()
+{
+	int x;
+	
+	cin >> x;
+
+	for (int i = 1; i <= x; ++i)
+	{
+		for (int j = x; j > i; --j)
+		{
+			cout << " ";
+		}
+		for (int j = 0; j < i; ++j)
+		{
+			cout << "*";
+		}
+		cout << '\n';
+	}
+
+
+}
