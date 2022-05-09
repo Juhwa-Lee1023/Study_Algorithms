7d458904e81faff3ba1aff5c7ebc9fbce7fd7479
Juhwa-Lee1023 <63584245+Juhwa-Lee1023@users.noreply.github.com>
2022-05-09 22:22:59 +0900

    [Bronze III] Title: A+B - 4, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+	
+
+int main(int argc, char const *argv[])
+{
+	int x = 0, y = 0;
+
+	while(cin >> x >> y)
+	{
+		cout << x + y << "\n";
+	}
+	
+	return 0;
+}
