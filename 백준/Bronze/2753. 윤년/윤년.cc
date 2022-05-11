9564d2aada0629123e31ffb988d4f8436c6a867e
Juhwa-Lee1023 <63584245+Juhwa-Lee1023@users.noreply.github.com>
2022-05-11 22:26:16 +0900

    [Bronze IV] Title: 윤년, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+
+
+using namespace std;
+
+int main()
+{
+	int x;
+	
+	cin >> x;
+
+	if (x % 4 == 0 && x % 100 != 0)
+	{
+		cout << 1;
+	}
+	else if(x % 400 == 0)
+	{
+		cout << 1;
+	}
+	else
+	{
+		cout << 0;
+	}
+	
+
+	return 0;
+
+}
