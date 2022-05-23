f3fbd1f5f5919f477307e82719ce77ff2305a6bb
Juhwa Lee <63584245+Juhwa-Lee1023@users.noreply.github.com>
2022-05-23 23:12:26 +0900

    [Bronze III] Title: A+B - 7, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{	
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+	int x;
+
+	cin >> x;
+
+	int a, b;
+
+	for (int i = 1; i <= x; ++i)
+	{
+		cin >> a >> b;
+		cout << "Case #" << i << ": " << a + b << "\n";
+	}
+
+
+	return 0;
+}
