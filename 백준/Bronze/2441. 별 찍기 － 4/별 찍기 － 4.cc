ee77a4eac933c5052f6f669e7a2bae83669429e4
Juhwa Lee <63584245+Juhwa-Lee1023@users.noreply.github.com>
2022-05-27 22:55:18 +0900

    [Bronze III] Title: 별 찍기 - 4, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+
+
+using namespace std;
+
+int main()
+{
+	int x;
+
+	cin >> x;
+
+	for (int i = 1; i <= x; ++i)
+	{
+		for (int j = 1; j < i; ++j)
+		{
+			cout << " ";
+		}
+		for (int k = x; k >= i; --k)
+		{
+			cout << "*";
+		}
+		cout << "\n";
+	}
+
+	return 0;
+}
