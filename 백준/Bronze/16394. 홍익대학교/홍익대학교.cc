300f6278291e9b7c398451bee43e8a9f15783c28
Juhwa Lee <juhwa.lee1023@gmail.com>
2022-06-10 01:35:48 +0900

    [Bronze V] Title: 홍익대학교, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/16394. 홍익대학교/홍익대학교.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int x = 1946, y;
+
+	cin >> y;
+
+	x = y - x;
+
+	cout << x;
+
+	return 0;
+	
+}
