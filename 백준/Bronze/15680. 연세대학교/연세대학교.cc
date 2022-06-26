f97656c360cee7bf0fc60aecb5eb956b5984db74
Juhwa Lee <juhwa.lee1023@gmail.com>
2022-06-26 23:19:00 +0900

    [Bronze IV] Title: 연세대학교, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+
+
+using namespace std;
+
+int main()
+{
+	int x;
+
+	cin >> x;
+
+	if (x == 0)
+	{
+		cout << "YONSEI";
+	}
+	else
+	{
+		cout << "Leading the Way to the Future";
+	}
+	
+
+	return 0;
+}
