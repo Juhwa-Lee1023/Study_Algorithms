aa8166477deaf5229561b0cf9b7930c90e49d117
Juhwa Lee <juhwa.lee1023@gmail.com>
2022-11-26 03:03:20 +0900

    [Silver III] Title: 소수 구하기, Time: 16 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+#define max 1000000
+
+int arr[max + 1]{ 0, };
+
+int main(void) {
+	ios::sync_with_stdio;
+	cin.tie(NULL);
+
+	int m = 0, n = 0;
+	cin >> m >> n;
+
+	for (int i = 2; i <= n; i++) { arr[i] = i; }
+
+    for (int i = 2; i * i <= n; i++) {
+        if (arr[i] == 0) { continue; }
+        for (int j = i * i; j <= n; j += i) { arr[j] = 0; }
+    }
+
+    for (int i = m; i <= n; i++) {
+        if (arr[i] != 0) { cout << arr[i] << '\n'; }
+    }
+    
+    return 0;
+
+}
