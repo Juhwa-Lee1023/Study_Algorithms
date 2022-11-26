a002d83ef35f1d76c35b20b7ce6f44b429222eeb
Juhwa Lee <juhwa.lee1023@gmail.com>
2022-11-27 01:39:42 +0900

    [level 1] Title: 정수 내림차순으로 배치하기, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+bool compare(int a, int b) {
+	return a > b;
+}
+
+long long solution(long long n) {
+    string str = to_string(n);
+    sort(str.begin(), str.end(), compare);
+    
+    return stod(str);
+}
