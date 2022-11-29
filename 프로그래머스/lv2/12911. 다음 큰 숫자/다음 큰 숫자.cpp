c9bce452d239cfc27624813c3a5a5d8b4088af67
Juhwa Lee <juhwa.lee1023@gmail.com>
2022-11-30 05:23:17 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.01 ms, Memory: 3.8 MB -BaekjoonHub

--- 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.cpp
+++ 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.cpp
@@ -3,34 +3,27 @@
 
 using namespace std;
 
-string to_binary(int num) {
+int binaryCount(int num) {
     string str = "";
+    int n = 0;
     while(num != 0) {
         str = (num % 2 == 0 ? "0" : "1") + str; 
         num /= 2;
     }
-    return str;
+    for(int i = 0; i < str.size(); i++) {
+        if(str[i] == '1') {
+            n++;
+        }
+    }
+    return n;
 }
 
 int solution(int n) {
-    int answer = 0;
-    string str = to_binary(n);
-    for(int i = 0; i < str.size(); i++) {
-        if(str[i] == '1') {
-            answer++;
-        }
-    }
+    int answer = binaryCount(n);
     
     while(true) {
         n++;
-        string str = to_binary(n);
-        int num = 0;
-        for(int i = 0; i < str.size(); i++) {
-            if(str[i] == '1') {
-                num++;
-            }
-        }
-        if(num == answer) {
+        if(answer == binaryCount(n)) {
             return n;
         }
     }
